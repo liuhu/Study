2f77bd69d27798119e4262691523bdcb75652b47
liuhu <mail.liuhu@gmail.com>
2015-04-14 16:40:26 +0800

    test HashMap

---
+++ study-demo/src/main/java/me/liuhu/study/demo/map/TestHashMap.java
@@ -0,0 +1,19 @@
+package me.liuhu.study.demo.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 版      权:  江苏千米网络科技有限公司 <br>
+ * 包      名:  me.liuhu.study.demo.map       <br>
+ * 描      述:                        <br>
+ * 创 建 人 :  LiuHu(OF1558)          <br>
+ * 修改时间:  2015/4/14                  <br>
+ */
+public class TestHashMap {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("a","a");
+        System.out.print(map);
+    }
+}
