57778778a68bf93bd96f1d74479bcb8689007777
liuhu <mail.liuhu@gmail.com>
2015-05-04 12:12:15 +0800

    dubbo study

--- study-dubbo-client/src/main/java/me/liuhu/BigDecimalTest.java
+++ study-dubbo-client/src/main/java/me/liuhu/BigDecimalTest.java
@@ -1,5 +1,9 @@
 package me.liuhu;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.math.BigDecimal;
+
 /**
  * 版      权:  江苏千米网络科技有限公司 <br>
  * 包      名:  me.liuhu       <br>
@@ -8,4 +12,15 @@ package me.liuhu;
  * 修改时间:  2015/4/27                  <br>
  */
 public class BigDecimalTest {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"spring-config.xml"});
+        Provider provider = context.getBean(Provider.class);
+        BigDecimal a = new BigDecimal(0.001);
+        BigDecimal b = new BigDecimal(0.001);
+        if (provider.compareBigDecimal(a, b)){
+            System.out.println("两值相等");
+        }else {
+            System.out.println("不等");
+        }
+    }
 }
